73f9a87ed86238b06b15ccb33fbbc1194e392a8f
PB-vt <cv.juanp@gmail.com>
2022-03-21 10:33:13 -0700

    Set webpack and neccesary dependecies for the project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean:true,
+  },
+  devtool: 'inline-source-map',
+  devServer:{
+    static: './dist',
+    open:true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/temp.html')
+
+    }),
+  ],
+ module: {
+   rules: [
+     {test: /\.(?:ico|gif|jpg|png)$/i,
+      type : 'asset/resource'
+    },
+     {
+       test: /\.css$/i,
+       use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+       
+   ],
+ },
+};
