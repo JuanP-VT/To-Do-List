47570e81e4c095f02f11643b2fa9dee6ec8ab9f8
PB-vt <cv.juanp@gmail.com>
2022-03-31 13:19:51 -0700

    Styled the page to make it look better

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import style from   './style.css';
 import { initPage } from "./InitPage";
 import { loadEvents } from './LoadInitEvents';
 import { showProjectsInSidebar } from './showProjectsInSidebar';
+import { displayAllActivities } from './displayAllActivities';
 
 
 
 initPage()
 loadEvents()
 showProjectsInSidebar()
+displayAllActivities()
